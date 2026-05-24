8c30437ee24877d0d49b3606af50931810fe591c
maryamjumah <jumahmaryam95@gmail.com>
2026-05-24 09:28:54 +0300

    Create EditTaskModal.jsx

---
+++ FE_task_manager/src/FE_task_manager/src/components/FE_task_manager/EditTaskModal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EditTaskModal() {
+  return (
+    <div>
+      <h2>Edit Task Modal Placeholder</h2>
+      <p>This component will show a modal to edit an existing task.</p>
+    </div>
+  );
+}

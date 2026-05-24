5597af1a35656b0b5f275ef761baea8ddbb85099
maryamjumah <jumahmaryam95@gmail.com>
2026-05-24 09:21:09 +0300

    Create TaskTable.jsx

---
+++ FE_task_manager/src/FE_task_manager/src/components/TaskTable.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TaskTable() {
+  return (
+    <div>
+      <h2>Task Table Placeholder</h2>
+      <p>This component will display a list of tasks.</p>
+    </div>
+  );
+}
